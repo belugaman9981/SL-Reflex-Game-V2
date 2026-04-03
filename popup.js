aac07f087e44eb45f694ef1dc819363472caafaa
belugaman9981 <littledage@outlook.com>
2026-04-02 20:30:13 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -491,3 +491,4 @@ function sdkSubmitScore(){
 }
 
 function sdkUpdateNumpad(){const cnt=Array(10).fill(0);for(let r=0;r<9;r++)for(let c=0;c<9;c++)if(sdkPlayer[r][c])cnt[sdkPlayer[r][c]]++;document.querySelectorAll(".sdk-num").forEach(btn=>{const n=Number(btn.dataset.n);btn.classList.toggle("dim",cnt[n]>=9);});}
+ 
